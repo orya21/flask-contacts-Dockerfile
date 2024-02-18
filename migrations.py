1c1b47e72d767540b20bbe54e31c6bd6e860280e
gagishmagi <gagi.shmagi@gmail.com>
2024-02-18 17:42:38 +0200

    Fixed issues with contacts app

--- migrations.py
+++ migrations.py
@@ -1,21 +1,25 @@
-from models import db, Contact
+from models import app, db, Contact
 from faker import Factory
 
 fake = Factory.create()
 # Spanish
-#fake = Factory.create('es_ES')
+# fake = Factory.create('es_ES')
 # Reload tables
-db.drop_all()
-db.create_all()
-# Make 100 fake contacts
-for num in range(100):
-    fullname = fake.name().split()
-    name = fullname[0]
-    surname = ' '.join(fullname[1:])
-    email = fake.email()
-    phone = fake.phone_number()
-    # Save in database
-    mi_contacto = Contact(name=name, surname=surname, email=email, phone=phone)
-    db.session.add(mi_contacto)
+with app.app_context():
+    db.create_all()
+    # db.drop_all()
 
-db.session.commit()
+    # db.create_all()
+    # db.drop_all()
+    # Make 100 fake contacts
+    for num in range(100):
+        fullname = fake.name().split()
+        name = fullname[0]
+        surname = ' '.join(fullname[1:])
+        email = fake.email()
+        phone = fake.phone_number()
+        # Save in database
+        mi_contacto = Contact(name=name, surname=surname, email=email, phone=phone)
+        db.session.add(mi_contacto)
+
+    db.session.commit()
